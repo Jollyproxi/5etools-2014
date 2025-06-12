7b8d029e231e9429365aaf52892f8fc5313c8852
Jollyproxi <davide.giolitti@gmail.com>
2025-06-12 23:16:32 +0200

    switcharoo

--- open2014.cpp
+++ open2014.cpp
@@ -11,10 +11,14 @@ int main ()
     system("git pull");
     system("cd .."); 
     system("npm i");
-    system("npm run build:sw:prod");
     system("npm audit fix");
+    system("npm run build:sw:prod");
     system("git fetch upstream");
     system("git merge upstream/main");    
+    system("git pull"); 
+    system("cd img");
+    system("git pull");
+    system("cd .."); 
     system("start http://localhost:8214");   
     system("py -m http.server 8214");
     
